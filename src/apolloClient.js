6baecacc300b672d511264cb5529c12639e69dbb
SABARIRAM RAMPRAKASH PREMA <sabariramrp@gmail.com>
2025-08-15 15:07:15 +0530

    v2

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,10 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: `https://graphql.contentful.com/content/v1/spaces/prfgtj5q7sxx`,
+  uri: `https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}`,
   cache: new InMemoryCache(),
   headers: {
-    Authorization: `Bearer usxgkxCs2jNoBPOfpQDb0aenTp7EWNOq-Bl6CTcOoQk`,
+    Authorization: `Bearer ${process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN}`,
   },
 });
 
